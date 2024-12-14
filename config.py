d8cf9ae8a2e3c5d7dcaf5f2879ffb4ff3c086404
pompomxyz <freestff0077@gmail.com>
2024-12-14 14:41:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
-BOT_TOKEN = "8086191457:AAGyO8P3HWnPL0PeIotzLz4-dnfO6kocbMw"
-API_ID = "24994752"
-API_HASH = "1c9b10f27f4ab2811ed4f102cc005837"
+import os
 
-# Temporary storage for logged-in Mega credentials
-MEGA_CREDENTIALS = {"email": None, "password": None}
+BOT_TOKEN = os.getenv("8086191457:AAGyO8P3HWnPL0PeIotzLz4-dnfO6kocbMw")
+API_ID = os.getenv("24994752")
+API_HASH = os.getenv("1c9b10f27f4ab2811ed4f102cc005837")
+MEGA_CREDENTIALS = {
+    "email": os.getenv("MEGA_EMAIL"),
+    "password": os.getenv("MEGA_PASSWORD")
+}
