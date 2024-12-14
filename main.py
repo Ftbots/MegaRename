d2d94d53350f159916c420b7f9ff17b19ab0bbc4
pompomxyz <freestff0077@gmail.com>
2024-12-14 14:39:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from pyrogram import Client
 from config import BOT_TOKEN, API_ID, API_HASH
 from handlers import start, login, rename
@@ -11,4 +12,6 @@ login.register_handlers(app)
 rename.register_handlers(app)
 
 if __name__ == "__main__":
-    app.run()
+    # Run the app on port specified by Koyeb
+    port = int(os.environ.get("PORT", 8000))  # Default to 8000 if not set
+    app.run(host="0.0.0.0", port=port)
