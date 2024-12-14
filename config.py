ee889ce8f5cff5a20037a47b064b32c2966ac748
pompomxyz <freestff0077@gmail.com>
2024-12-14 14:58:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
-BOT_TOKEN = "8086191457:AAGyO8P3HWnPL0PeIotzLz4-dnfO6kocbMw"
-API_ID = "24994752"
-API_HASH = "1c9b10f27f4ab2811ed4f102cc005837"
+import os
 
-# Temporary storage for logged-in Mega credentials
-MEGA_CREDENTIALS = {"email": None, "password": None}
+# Correct way to get environment variables
+BOT_TOKEN = os.getenv("BOT_TOKEN")  # Environment variable name should be used here
+API_ID = os.getenv("API_ID")  # Same here
+API_HASH = os.getenv("API_HASH")  # Same here
+
+# MEGA credentials
+MEGA_CREDENTIALS = {
+    "email": os.getenv("MEGA_EMAIL"),
+    "password": os.getenv("MEGA_PASSWORD")
+}
