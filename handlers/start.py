2afa6a8250aa41b8c420ba6aea3fa158a58074f1
pompomxyz <freestff0077@gmail.com>
2024-12-14 13:27:01 +0530

    Delete handlers directory

--- handlers/start.py
+++
@@ -1,7 +0,0 @@
-from pyrogram import filters
-from pyrogram.types import Message
-
-def register_handlers(app):
-    @app.on_message(filters.command("start") & filters.private)
-    async def start(bot, message: Message):
-        await message.reply("Welcome to Mega Rename Bot!\nUse /login to log in to your Mega account.")
